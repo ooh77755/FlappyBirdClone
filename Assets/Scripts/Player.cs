44592c16f3bded713c1261b900ae09372060bc06
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-02 17:50:13 +0100

    Added 'CopterCrash()' and disallowed player input on collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,47 @@ public class Player : MonoBehaviour
 {
     float upwardsForce = 1000f;
 
+    bool isAlive = true;
+
     Rigidbody2D rb;
+    CircleCollider2D myCollider;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isAlive)
+        {
+            MakeCopterFly();
+        }
+
+        else if (!isAlive)
+        {
+            return;
+        }
+
+        CopterCrash();
+    }
+
+    private void MakeCopterFly()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector2.up * upwardsForce * Time.deltaTime);
+        }
+    }
+
+    private void CopterCrash()
+    {
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Pipe")))
+        {
+            isAlive = false;
         }
     }
 }
