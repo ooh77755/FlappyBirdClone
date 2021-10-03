671974981c717deec499f802a8674ce66403749d
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-03 19:41:12 +0100

    Instantiating randomly from an array added to the spawner script

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject pipePrefabTop;
-    [SerializeField] GameObject pipePrefabMiddle;
-    [SerializeField] GameObject pipePrefabBottom;
+    [SerializeField] GameObject[] pipeArray;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(pipePrefabMiddle, transform.position, Quaternion.identity);
+        int randomNumber = Random.Range(0, pipeArray.Length);
+        Instantiate(pipeArray[randomNumber], transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
