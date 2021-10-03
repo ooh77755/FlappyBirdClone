c18cee325ee47e6770d3050b78f00b70e064b6fa
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-03 19:29:21 +0100

    Created PipeSpawner, and associated script, and added instantiate debug code

---
+++ Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject pipePrefabTop;
+    [SerializeField] GameObject pipePrefabMiddle;
+    [SerializeField] GameObject pipePrefabBottom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(pipePrefabMiddle, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
