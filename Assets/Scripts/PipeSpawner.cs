0023ae2ffd36c5515e69381cab65883e235d2a94
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-04 19:11:23 +0100

    Pipes now instantiate, randomly, at set intervals

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -1,21 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PipeSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] pipeArray;
+    [SerializeField] float InstantiationTimer = 8f;
 
     // Start is called before the first frame update
     void Start()
     {
-        int randomNumber = Random.Range(0, pipeArray.Length);
+        int randomNumber = UnityEngine.Random.Range(0, pipeArray.Length);
         Instantiate(pipeArray[randomNumber], transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CreatePrefab();
+    }
+
+    private void CreatePrefab()
+    {
+        int randomNumber = UnityEngine.Random.Range(0, pipeArray.Length);
+        InstantiationTimer -= Time.deltaTime;
+        if (InstantiationTimer <= 0)
+        {
+            Instantiate(pipeArray[randomNumber], transform.position, Quaternion.identity);
+            InstantiationTimer = 8f;
+        }
     }
 }
