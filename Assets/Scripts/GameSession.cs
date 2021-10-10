f30c32c3695dbf41c26b26b546976d384aec573e
ooh77755 <86021255+ooh77755@users.noreply.github.com>
2021-10-10 16:44:35 +0100

    Add 'game session.cs' & UI TMP. Added code for player score to increase each sec. Player can see score increase over time

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    int pointsPerPipe = 1;
+    int currentScore = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = currentScore.ToString();
+        InvokeRepeating("AddToScore", 1, 1);
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerPipe;
+        scoreText.text = currentScore.ToString();
+    }
+}
